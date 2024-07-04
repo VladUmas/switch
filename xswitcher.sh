959529929e124e0ed6e34055b4cb8590b73cb849
VladUmas <57813260+VladUmas@users.noreply.github.com>
2024-07-04 16:17:43 +0300

    Update xswitcher.sh

--- xswitcher.sh
+++ xswitcher.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-# Клонирование репозитория
-echo "Клонируем репозиторий Xswitcher..."
 git clone https://github.com/VladUmas/Xswitcher
-
-# Переход в каталог Xswitcher
-echo "Переходим в каталог Xswitcher..."
 cd Xswitcher
-
-# Назначение прав на выполнение
-echo "Назначаем права на выполнение для xswitcher..."
 chmod +x x\ switcher/bin/xswitcher
-
-# Запуск xswitcher
 echo "Запускаем xswitcher..."
 ./x\ switcher/bin/xswitcher
