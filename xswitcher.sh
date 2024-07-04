a72699026f82764d406ef90b462e8c55997baa64
VladUmas <57813260+VladUmas@users.noreply.github.com>
2024-07-04 15:55:13 +0300

    Add files via upload

---
+++ xswitcher.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Установка git
+echo "Устанавливаем git..."
+apt-get update
+apt-get install -y git
+
+# Клонирование репозитория
+echo "Клонируем репозиторий Xswitcher..."
+git clone https://github.com/VladUmas/Xswitcher
+
+# Переход в каталог Xswitcher
+echo "Переходим в каталог Xswitcher..."
+cd Xswitcher
+
+# Назначение прав на выполнение
+echo "Назначаем права на выполнение для xswitcher..."
+chmod +x x\ switcher/bin/xswitcher
+
+# Запуск xswitcher
+echo "Запускаем xswitcher..."
+./x\ switcher/bin/xswitcher
